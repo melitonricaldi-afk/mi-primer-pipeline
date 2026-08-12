007c63ddd3257618641660af03718f68c62f976c
melitonricaldi-afk <melitonricaldi@gmail.com>
2026-08-12 00:03:42 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+def saludar():
+    print("----------------------------------------")
+    print("¡Hola desde el pipeline de GitHub Actions!")
+    print("----------------------------------------")
+
+if __name__ == "__main__":
+    saludar()
