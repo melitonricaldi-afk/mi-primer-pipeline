ec140083c8de81359e1ea488593ba646e853ef14
melitonricaldi-afk <melitonricaldi@gmail.com>
2026-08-12 00:10:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import sys
+
 def saludar():
     print("----------------------------------------")
     print("¡Hola desde el pipeline de GitHub Actions!")
+    print(f"Versión de Python ejecutándose: {sys.version}")
     print("----------------------------------------")
 
 if __name__ == "__main__":
